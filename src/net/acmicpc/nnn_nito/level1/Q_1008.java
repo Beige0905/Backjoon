b738e1a3629a97f9d2d542622c47f5cd4e322020
BegieYu <beige0905@gmail.com>
2022-08-10 21:08:34 +0900

    220810 ~18108

---
+++ src/net/acmicpc/nnn_nito/level1/Q_1008.java
@@ -0,0 +1,17 @@
+package net.acmicpc.nnn_nito.level1;
+
+import java.util.Scanner;
+
+public class Q_1008 {
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		double a,b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a/b);
+		
+	}
+}
---
+++ src/net/acmicpc/nnn_nito/level1/Q_10869.java
@@ -0,0 +1,22 @@
+package net.acmicpc.nnn_nito.level1;
+
+import java.util.Scanner;
+
+public class Q_10869 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int a, b;
+		a = sc.nextInt();
+		b = sc.nextInt();
+		
+		System.out.println(a+b);
+		System.out.println(a-b);
+		System.out.println(a*b);
+		System.out.println(a/b);
+		System.out.println(a%b);
+
+	}
+
+}
